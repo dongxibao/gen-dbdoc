1168581c7a0f3f59286b60edd57a589c0e510b46
e <dxb@winseeing.com>
2020-10-12 14:11:37 +0800

    feat:添加注释

--- src/main/java/top/dongxibao/gendoc/GenDbDocApplication.java
+++ src/main/java/top/dongxibao/gendoc/GenDbDocApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * screw有两种执行方式：
- *      第一种是pom文件配置
+ *      第一种是pom文件配置(配置好后在 Maven -> Plugins -> screw -> screw:run执行)
  *      第二种是代码执行
  */
 @SpringBootApplication
